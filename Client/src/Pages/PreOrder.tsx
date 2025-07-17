e9ed03a49bed278f218d8aeaba7a257402d7fcb3
Wai Kyaw Myo <waikyawmyo4u@gmail.com>
2025-07-17 16:29:21 +0700

    Pre-order/V1

--- Client/src/Pages/PreOrder.tsx
+++ Client/src/Pages/PreOrder.tsx
@@ -1,14 +1,154 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useGetAllMenuMutation } from "../Slice/API/userApi";
+import { useEffect, useState } from "react";
+import { faFire } from "@fortawesome/free-solid-svg-icons";
 
 function PreOrder() {
+  const [getall] = useGetAllMenuMutation();
+  const [menu, setMenu] = useState([]);
+  const [set, setSet] = useState([]);
+  const [activeSection, setActiveSection] = useState("popular");
 
+  useEffect(() => {
+    const get = async () => {
+      const res = await getall({});
+      console.log(res);
+      setMenu(res.data.menu);
+      setSet(res.data.sets);
+    };
+    get();
+  }, [getall]);
+  const handleSectionClick = (section) => {
+    setActiveSection(section);
+  };
   return (
-    <div className="bg-amber-400 w-full h-[400px] mx-auto lg:w-[800px]">
-        <div>
-            
+    <div className=" w-fullh-[400px] mx-auto lg:w-[800px]">
+      <div className="w-full mx-auto ">
+        <div className="">
+          <nav className="flex justify-evenly items-center border-b  ">
+            <button
+              onClick={() => handleSectionClick("popular")}
+              className={`text-xl p-3 px-5 ${
+                activeSection === "popular" ? "border-b-2" : ""
+              }`}
+            >
+              Popular
+            </button>
+            <button
+              onClick={() => handleSectionClick("set")}
+              className={`text-xl p-3 px-5 ${
+                activeSection === "set" ? "underline" : ""
+              }`}
+            >
+              Set
+            </button>
+            <button
+              onClick={() => handleSectionClick("menu")}
+              className={`text-xl p-3 px-5 ${
+                activeSection === "menu" ? "border-b " : ""
+              }`}
+            >
+              Menu
+            </button>
+          </nav>
         </div>
-
+      </div>
+      <div className="h-[600px] overflow-y-scroll">
+        <h1 className="px-3 sm:px-6  text-2xl font-semibold py-2 sm:py-3 ">
+        <FontAwesomeIcon icon={faFire} /> Popular
+      </h1>
+      <div className="px-3 sm:px-6 ">
+        {set.length &&
+          set.map((s, index) => (
+            <div key={index} className="py-1 flex justify-between sm:py-3 border-b ">
+              <div className="flex items-center gap-3">
+              <img src={s.image} className="w-30" alt="" />
+              <div className="w-[180px] sm:w-[300px]">
+                <h1 className="font-semibold text-xl">{s.name}</h1>
+                {s.sets.map((m) => {
+                  console.log(m.menu);
+                  return (
+                    <>
+                    <span key={m._id}>
+                      {" "}
+                      (x{m.unit_Quantity}) {m.menu.name}
+                    </span>
+                    
+                    </>
+                  );
+                })}
+                <p>{s.price} MMK</p>
+              </div>
+              </div>
+              <div>
+                <button>
+                  button
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
+      <h1 className="px-3 sm:px-6  text-2xl font-semibold py-2 sm:py-3 ">
+         Set
+      </h1>
+      <div className="px-3 sm:px-6 ">
+        {set.length &&
+          set.map((s, index) => (
+            <div key={index} className="py-1 flex justify-between sm:py-3 border-b">
+              <div className="flex items-center gap-3">
+              <img src={s.image} className="w-30" alt="" />
+              <div className="w-[180px] sm:w-[300px]">
+                <h1 className="font-semibold text-xl">{s.name}</h1>
+                {s.sets.map((m) => {
+                  console.log(m.menu);
+                  return (
+                    <>
+                    <span key={m._id}>
+                      {" "}
+                      (x{m.unit_Quantity}) {m.menu.name}
+                    </span>
+                    
+                    </>
+                  );
+                })}
+                <p>{s.price} MMK</p>
+              </div>
+              </div>
+              <div>
+                <button>
+                  button
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
+      <h1 className="px-3 sm:px-6  text-2xl font-semibold py-2 sm:py-3 ">
+         Menu
+      </h1>
+      <div className="px-3 sm:px-6 ">
+        {menu.length &&
+          menu.map((s, index) => (
+            <div key={index} className="py-1 flex justify-between sm:py-3 border-b">
+              <div className="flex items-center gap-3">
+              <img src={s.image} className="w-30 h-20 bg-center object-cover bg-no-repeat overflow-hidden" alt="" />
+              <div className="w-[180px] sm:w-[300px]">
+                <h1 className="font-semibold text-xl">{s.name}</h1>
+                
+                <p>{s.price} MMK</p>
+              </div>
+              </div>
+              <div>
+                <button>
+                  button
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
+      </div>
+      
     </div>
-  )
+  );
 }
 
-export default PreOrder
+export default PreOrder;
--- Server/src/controllers/showMenu.ts
+++ Server/src/controllers/showMenu.ts
@@ -10,16 +10,36 @@ export const getAllMenu = asyncHandler(async (req: Request, res: Response) => {
     {
       $lookup: {
         from: "set_menus",
-        localField: "_id",
-        foreignField: "set_id",
-        as: "menus",
+        let: { setId: "$_id" },
+        pipeline: [
+          { $match: { $expr: { $eq: ["$set_id", "$$setId"] } } },
+          {
+            $lookup: {
+              from: "menus",
+              localField: "menu_id",
+              foreignField: "_id",
+              as: "menu",
+            },
+          },
+          {
+            $unwind: "$menu",
+          },
+          {
+            $project: {
+              _id: 0,
+              unit_Quantity: 1,
+              menu: 1,
+            },
+          },
+        ],
+        as: "sets",
       },
     },
   ]);
 
-  if (!menu && !sets ) {
+  if (menu.length === 0 && sets.length === 0) {
      res.status(404).json({ message: "No menu or sets found" });
   }
 
-   res.status(200).json({ menu: menu, sets: sets});
+  res.status(200).json({ menu: menu, sets: sets });
 });
